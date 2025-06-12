49a270270b01ab976b598f3c306995a7ec6f3a76
soyuz43 <kebekad673@proton.me>
2025-06-12 09:56:28 -0500

    fix(error) fixed error in navigation

--- client/src/components/userprofiles/UserProfileDetails.jsx
+++ client/src/components/userprofiles/UserProfileDetails.jsx
@@ -1,49 +1,55 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProfile } from "../../managers/userProfileManager";
-import { createSubscription, deleteSubscription, getAllSubscriptions } from "../../managers/subscriptionManager";
+import {
+  createSubscription,
+  deleteSubscription,
+  getAllSubscriptions,
+} from "../../managers/subscriptionManager";
 
-export default function UserProfileDetails({loggedInUser}) {
+export default function UserProfileDetails({ loggedInUser }) {
   const [userProfile, setUserProfile] = useState();
-  const [ subs, setSubs] = useState([]);
-  const [ allSubs, setAllSubs]=useState([]);
-  
+  const [subs, setSubs] = useState([]);
+  const [allSubs, setAllSubs] = useState([]);
 
   const { id } = useParams();
   useEffect(() => {
     getProfile(id).then(setUserProfile);
   }, [id]);
 
-  useEffect(() =>
- getAllSubscriptions().then(res => {
-  setAllSubs(res)
-  const subs = res.filter(s => s.authorId == id)
-    setSubs(subs)
-  }), [id])
+useEffect(() => {
+  getAllSubscriptions().then(res => {
+    setAllSubs(res);
+    setSubs(res.filter(s => s.authorId == id));
+  });
+}, [id]);
+
 
   const handleSub = () => {
-      const isSub = allSubs.find(sub => sub.subscriberId == loggedInUser.id && sub.authorId == id);
-     if (isSub) {
-    deleteSubscription(isSub.id).then(() => {
-      getAllSubscriptions().then(res => {
-        setAllSubs(res);
-        setSubs(res.filter(s => s.authorId == id));
+    const isSub = allSubs.find(
+      (sub) => sub.subscriberId == loggedInUser.id && sub.authorId == id
+    );
+    if (isSub) {
+      deleteSubscription(isSub.id).then(() => {
+        getAllSubscriptions().then((res) => {
+          setAllSubs(res);
+          setSubs(res.filter((s) => s.authorId == id));
+        });
       });
-    });
-  } else {
-    const subInfo = {
-      SubscriberId: loggedInUser.id,
-      AuthorId: parseInt(id)
-    };
+    } else {
+      const subInfo = {
+        SubscriberId: loggedInUser.id,
+        AuthorId: parseInt(id),
+      };
 
-        createSubscription(subInfo).then(() => {
-      getAllSubscriptions().then(res => {
-        setAllSubs(res);
-        setSubs(res.filter(s => s.authorId == id));
+      createSubscription(subInfo).then(() => {
+        getAllSubscriptions().then((res) => {
+          setAllSubs(res);
+          setSubs(res.filter((s) => s.authorId == id));
+        });
       });
-      })
-  }
-  }
+    }
+  };
   if (!userProfile) {
     return null;
   }
@@ -52,9 +58,8 @@ export default function UserProfileDetails({loggedInUser}) {
       <img src={userProfile.imageLocation} alt={userProfile.firstName} />
       <h3>{userProfile.fullName}</h3>
       <p>Username: {userProfile.userName}</p>
-      <button onClick={handleSub}>Sub</button> 
-      <p>Sub. Count: {subs.length }</p>
-         </>
+      <button onClick={handleSub}>Sub</button>
+      <p>Sub. Count: {subs.length}</p>
+    </>
   );
-
 }
